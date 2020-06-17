c17fd998a87877374b5478551e1a98e04156b869
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-06-18 00:32:06 +0200

    Add TodoListItem content test

---
+++ test/unit/specs/TodoListItem.spec.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import TodoListItem from '@/components/TodoListItem';
+import { mount } from '@vue/test-utils'
+
+
+describe('TodoListItem.vue', () => {
+  it('should render correct contents', async () => {
+    const todo = {
+      text: 'Todo text A',
+    };
+
+    const wrapper = mount(TodoListItem, {
+      propsData: { todo },
+    })
+
+    expect(wrapper.exists()).toBeTruthy();
+
+    const todoListItem = wrapper.findComponent(TodoListItem);
+    expect(todoListItem.exists()).toBeTruthy();
+    expect(todoListItem.html()).toContain(todo.text);
+  });
+});
