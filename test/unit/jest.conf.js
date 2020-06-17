14732da130f4b668180b63dc29e0cbdf3556bf13
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-06-18 00:31:44 +0200

    Use jsdom instead of node, add missing testURL to fix error

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -14,9 +14,9 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
+  transformIgnorePatterns: ['/node_modules/'],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
@@ -24,5 +24,6 @@ module.exports = {
     '!src/router/index.js',
     '!**/node_modules/**',
   ],
-  testEnvironment: 'node',
+  testURL: 'http://localhost',
+  testEnvironment: 'jsdom',
 };
