0edef7d12d0288792d192e56ae058522fe9e865c
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-06-17 23:32:26 +0200

    Create TodoListItem component

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="root">
+    <h6 class="title">{{ todo.text }}</h6>
+    <span class="remove-button" v-html="xMark" @click="clickedRemoveButton"></span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      xMark: '&#x2716;',
+    };
+  },
+  methods: {
+    clickedRemoveButton() {
+      this.$emit('clickedRemoveButton', this.todo.text);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.root {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.title {
+  margin-right: 8px;
+}
+.remove-button {
+  padding: 8px;
+  background: #CCCCCC;
+  border-radius: 50%;
+}
+</style>
